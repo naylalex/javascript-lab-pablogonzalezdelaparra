5fbda31c8e57834e1316531a94b57c6fa4c1ef72
Pablo González de la Parra <a01745096@itesm.mx>
2022-02-28 16:50:18 +0000

    Realización del primer laboratorio de JavaScript

--- labs/custom_button/student/update_player.js
+++ labs/custom_button/student/update_player.js
@@ -1,11 +1,13 @@
 /* LAB: Asigna a la variable 'para' una referencia al elemento p por medio de document.querySelector() */
-
+const para = document.querySelector('p');
 /* LAB: Añade un event listener para el evento click asociado a la función updateName */
-
+para.addEventListener('click', updateName);
 /* LAB: Implementa la función updateName() considerando:
 Presentación de un diálogo por medio de la función prompt()
 Asignación del contenido de texto del párrafo por medio del atributo textContent de la variable para
 para.textContent = 'Some string'
 */
 function updateName() {
+    const name = prompt('Enter a new drummer');
+    para.textContent = `Drummer 1: ${name}`;
 }
